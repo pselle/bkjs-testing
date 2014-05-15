1963f11cb13726d280e70f53ca3860f858f93f7e
Pamela Selle <pamela_selle@cable.comcast.com>
2014-05-15 12:09:37 -0400

    Put mocha test in its own folder

---
+++ demo/mocha/test/test.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+var cats = require('../cats.js');
+
+describe("An area of my application", function() {
+    it("should know that 2 and 2 is 4", function(){
+       assert.equal(4, 2+2);
+    });
+});
+
+describe("cats", function() {
+    it("should be able to mae a cat", function() {
+        var c = new Cat();
+        assert.equal(typeof(c), 'object');
+    });
+
+    it("should assign a default name", function() {
+        var c = new Cat();
+        assert.equal(c.name, 'Fluffy');
+    });
+});
