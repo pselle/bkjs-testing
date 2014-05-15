ab8417ef9e90672725f08abcd3e966e1a68e99a2
Pamela Selle <pamela_selle@cable.comcast.com>
2014-05-15 11:45:42 -0400

    Add demo files for jasmine and mocha

---
+++ demo/test/test.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+describe("An area of my application", function() {
+    it("should know that 2 and 2 is 4", function(){
+       assert.equal(4, 2+2);
+    });
+});
+
